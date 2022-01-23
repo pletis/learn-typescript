edbb023da202510bc96acbe130f225e4ff1a02d9
donghyune <tjswk70000@naver.com>
2022-01-23 23:00:43 +0900

    second commit

---
+++ class-note/3_interface.ts
@@ -0,0 +1,67 @@
+interface User {
+    age: number;
+    name: string;
+}
+
+// 변수에 인터페이스 활용
+var seho: User = {
+    age: 26,
+    name: "동현"
+}
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user)
+}
+const capt: User = {
+    name: '캡틴',
+    age: 100
+}
+getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+var sum: SumFunction;
+sum = function(a: number, b: number): number {
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+
+var arr: StringArray = ['a','b','c'];
+// arr[0] = "10";
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp;
+}
+
+var obj: StringRegexDictionary = {
+    // sth: /abc/,
+    cssFile: /\.css$/,
+    jsFile: /\.js$/
+}
+
+Object.keys(obj).forEach(function(value){})
+
+// 인터페이스 확장
+interface Person {
+    name: string;
+    age: number;
+}
+
+interface Developer extends Person {
+  language: string;
+}
+
+var captain: Developer = {
+    language: "ts",
+    name: "박동현",
+    age: 26
+}
