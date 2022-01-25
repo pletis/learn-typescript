f4b6ddfa9f123ddf389b5c017595dde2496c2ba8
donghyune <tjswk70000@naver.com>
2022-01-25 17:31:29 +0900

    class

---
+++ class-note/class.js
@@ -0,0 +1,17 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+} 
+var capt = new Person('캡틴', 100);
+
+class Person {
+    // 클래스 로직
+    constructor(name, age) {
+        console.log('생성 되었습니다.');
+        this.name = name;
+        this.age = age;
+    }
+}
+
+var dong = new Person('동현', 26);
+console.log(dong);
